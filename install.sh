23e8dc2000536a8f7e6062a1bbc7f2d38e30723b
Cherry Ng <cherry92@gmail.com>
2015-05-30 12:44:10 +0800

    install pathogen script

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# install pathogen
+mkdir -p $DIR/.vim/autoload $DIR/.vim/bundle
+curl -LSso $DIR/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 # compile modules
 git submodule update --init --recursive
 ./.vim/bundle/YouCompleteMe/install.sh --clang-completer --gocode-completer
