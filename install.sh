5a58ddca583479a7f66096d4702a95a7e0db04fb
Cherry Ng <cherry92@gmail.com>
2015-05-30 13:56:06 +0800

    remove dot before file/dir names

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ curl -LSso $DIR/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # compile modules
 git submodule update --init --recursive
-./.vim/bundle/YouCompleteMe/install.sh --clang-completer --gocode-completer
+./vim/bundle/YouCompleteMe/install.sh --clang-completer --gocode-completer
 
 # link scripts
-ln -s $DIR/.vim ~/.vim
-ln -s $DIR/.vimrc ~/.vimrc
-ln -s $DIR/.gitconfig ~/.gitconfig
-ln -s $DIR/.gitignore-global ~/.gitignore
+ln -s $DIR/vim ~/.vim
+ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/gitconfig ~/.gitconfig
+ln -s $DIR/gitignore ~/.gitignore
