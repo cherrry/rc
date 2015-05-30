13c7842f1e699520d88a1b36be4050586053a842
Cherry Ng <cherry92@gmail.com>
2015-05-30 12:41:50 +0800

    update rc links

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 # compile modules
 git submodule update --init --recursive
 ./.vim/bundle/YouCompleteMe/install.sh --clang-completer --gocode-completer
 
 # link scripts
-ln -s ./.vim ~/.vim
-ln -s ./.vimrc ~/.vimrc
+ln -s $DIR/.vim ~/.vim
+ln -s $DIR/.vimrc ~/.vimrc
